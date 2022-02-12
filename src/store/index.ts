36bcc518a9e547d20b429ddcd50bee44605888c3
ryota-09 <n.ryo950911@gmail.com>
2022-02-12 15:13:01 +0900

    キー判定機能確認OK

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,11 +4,23 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: true,
   state: {
-  },
-  mutations: {
+    variableList: ["id", "name", "image", "gender", "mailAddress", "errorMessage"]
   },
   actions: {
+  },
+  mutations: {
+    addVariables(state){
+      return (newVariable: string) => {
+        state.variableList.push(newVariable);
+      }
+    }
+  },
+  getters: {
+    getVariableArray(state): Array<string>{
+      return state.variableList;
+    },
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="title"><h1>Typing Variables</h1></div>
+    <div class="about"><span>テキストが入ります。</span></div>
+    <div class="variableList"><span>ここに一覧表を表示します。</span></div>
+    <div class="display-field">
+      <p class="count">60</p>
+      <div class="wrap">
+        <p class="variable-text">
+          <span v-for="text of targetTextArray" v-bind:key="text">
+            <span class="each-str">{{ text }}</span>
+          </span>
+        </p>
+      </div>
+      <div class="start-button">
+        <span v-on:click="startButton" v-on:keydown="startButton">ボタン</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+@Component({})
+export default class Home extends Vue {
+  private targetTextArray = Array<string>();
+  private pressedKey = "";
+  private slicedStr = "";
+
+  startButton(): void {
+    this.createText();
+    this.keyDown();
+  }
+
+  createText(): void{
+    let ran = Math.floor(Math.random() * this.variableArray.length);
+    this.targetTextArray = this.variableArray[ran].split("");
+  }
+
+  keyDown(): void {
+    addEventListener("keydown", (event) => {
+      this.pressedKey = event.key;
+      this.checkStr(this.pressedKey, this.targetTextArray[0]);
+    });
+  }
+
+  checkStr(clickedKey: string, targetTextStr: string): void{
+    if ( clickedKey === targetTextStr ){
+      console.log("key正解");
+    } else {
+      console.log("間違い");
+      return;
+    }
+  }
+
+  get variableArray(): Array<string> {
+    return this.$store.getters.getVariableArray;
+  }
+}
 </script>
+<style scoped>
+.display-field {
+  font-size: 6em;
+  text-align: center;
+}
+.count {
+  margin: 0;
+  font-weight: bold;
+  color: #888;
+}
+.wrap {
+  margin-top: 20px;
+  padding: 20px 10px;
+  background-color: #a4b0be;
+  font-weight: bold;
+  color: #fff;
+}
+</style>
